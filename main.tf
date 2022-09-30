edd21ca50734f6afabba551a5001b3b70627c7d5
nagaroony <nagendrababu7248@gmail.com>
2022-09-30 09:47:06 -0500

    updated main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "www-naga"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "dem0-getub-actions"
     }
   }
 }
